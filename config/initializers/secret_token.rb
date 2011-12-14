e94299450cfe11ae46788a8c9690ed59cdfe324b
Rudi Engelbrecht <rudi@engelbrecht.me>
2011-12-14 15:51:01 +0200

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebPlay::Application.config.secret_token = '54d40df932c76b8b105c16afdbf61b56058ceb813bd3da5dcb53b70d8d3eec2256c11fc44b5d522c84bc189475092ad41586e4819a4645dd4f4c67c32f5f41b6'
